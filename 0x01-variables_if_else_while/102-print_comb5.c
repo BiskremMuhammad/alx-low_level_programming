f46a032650a8338bbe5aa49c00ef1e464bffde59
Muhammad Omran <use.yurMind@gmail.com>
2023-06-16 14:50:35 -0700

    0x01-102 [optional] two two-numbers combination

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: 0 void
+ */
+int main(void)
+{
+	int i, j, a, b;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (a = 0; a <= '9'; a++)
+			{
+				for (b = '0'; b <= '9'; b++)
+				{
+					if ((i+j) != (a+b) && (i+j) < (a=b))
+					{
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(a);
+						putchar(b);
+
+						if ((i+j) < 'r' && (a+b) <= 'r')
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
