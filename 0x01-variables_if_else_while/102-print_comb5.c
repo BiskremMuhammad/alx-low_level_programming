714e451384a52e5b57ea799f646723106ea41b01
Muhammad Omran <use.yurMind@gmail.com>
2023-06-16 15:39:51 -0700

    0x01-102 [optional] fix logic and refactore

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,13 +1,42 @@
 #include <stdio.h>
 
 /**
+ * compareAndPrint - To Compare values and Print
+ * @i: number #1 tens multiplcation
+ * @j: number #1 under 10 digit
+ * @a: number #2 tens multiplcation
+ * @b: number #2 under 10 digit
+ */
+void compareAndPrint(int i, int j, int a, int b)
+{
+	int ij, ab;
+
+	ij = (10 * i) + j;
+	ab = (10 * a) + b;
+	if (ab > ij)
+	{
+		putchar(i);
+		putchar(j);
+		putchar(' ');
+		putchar(a);
+		putchar(b);
+
+		if ((i + j) < 'q' && (a + b) <= 'r')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+}
+
+/**
  * main - Entry Point
  *
  * Return: 0 void
  */
 int main(void)
 {
-	int i, j, a, b, ij, ab;
+	int i, j, a, b;
 
 	for (i = '0'; i <= '9'; i++)
 	{
@@ -17,22 +46,7 @@ int main(void)
 			{
 				for (b = '0'; b <= '9'; b++)
 				{
-					ij = (10 * i) + j;
-					ab = (10 * a) + b;
-					if ((i + j) != (a + b) && ab > ij)
-					{
-						putchar(i);
-						putchar(j);
-						putchar(' ');
-						putchar(a);
-						putchar(b);
-
-						if ((i + j) < 'q' && (a + b) <= 'r')
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
+					compareAndPrint(i, j, a, b);
 				}
 			}
 		}
