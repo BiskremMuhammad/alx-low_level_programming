7ac56d61196c02afcccfae80db98670453f99c82
Muhammad Omran <use.yurMind@gmail.com>
2023-06-16 08:37:27 -0700

    0x01-02 print alphabit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: 0 void
+ */
+int main (void)
+{
+	string alpha[] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+	for (i = 0; i < strlen(alpha); i++)
+	       putchar(alpha[i]);
+	putchar('\n');	
+}
