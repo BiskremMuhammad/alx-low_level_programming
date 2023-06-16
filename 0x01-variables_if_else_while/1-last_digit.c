59895143150cc081b85635afeb01045b1d9fd26c
Muhammad Omran <use.yurMind@gmail.com>
2023-06-16 08:45:43 -0700

    0x01-01 declare all variables in one line

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,11 +9,10 @@
  */
 int main(void)
 {
-	int n;
+	int n, ldOfn;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int ldOfn;
 	ldOfn = n % 10;
 	if (ldOfn == 0)
 		printf("Last digit of %d is %d and is 0\n", n, ldOfn);
