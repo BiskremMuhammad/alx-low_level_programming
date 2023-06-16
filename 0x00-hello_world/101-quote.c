26e005a2649fd515f9753c666a598ae9ea28b77e
Muhammad Omran <use.yurMind@gmail.com>
2023-06-16 06:39:55 -0700

    0x00-101 [optional] use putchar

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,64 +7,88 @@
  */
 int main(void)
 {
-	putchar('a');
-	putchar('n');
-	putchar('d');
-	putchar(' ');
-	putchar('t');
-	putchar('h');
-	putchar('a');
-	putchar('t');
-	putchar(' ');
-	putchar('p');
-	putchar('i');
-	putchar('e');
-	putchar('c');
-	putchar('e');
-	putchar(' ');
-	putchar('o');
-	putchar('f');
-	putchar(' ');
-	putchar('a');
-	putchar('r');
-	putchar('t');
-	putchar(' ');
-	putchar('i');
-	putchar('s');
-	putchar(' ');
-	putchar('u');
-	putchar('s');
-	putchar('e');
-	putchar('f');
-	putchar('u');
-	putchar('l');
-	putchar('"');
+	printQuote();
 	putchar(' ');
 	putchar('-');
 	putchar(' ');
-	putchar('D');
-	putchar('o');
-	putchar('r');
-	putchar('a');
-	putchar(' ');
-	putchar('K');
-	putchar('o');
-	putchar('r');
-	putchar('p');
-	putchar('a');
-	putchar('r');
-	putchar(',');
-	putchar(' ');
-	putchar('2');
-	putchar('0');
-	putchar('1');
-	putchar('5');
-	putchar('-');
-	putchar('1');
-	putchar('0');
-	putchar('-');
-	putchar('1');
-	putchar('9');
+	printAuthor();
 
 	return (1);
+}
+
+/**
+ * printQuote - to print the quote part
+ *
+ * Return: 0 void
+ */
+function printQuote(void)
+{
+	putchar('a');
+        putchar('n');
+        putchar('d');
+        putchar(' ');
+        putchar('t');
+        putchar('h');
+        putchar('a');
+        putchar('t');
+        putchar(' ');
+        putchar('p');
+        putchar('i');
+        putchar('e');
+        putchar('c');
+        putchar('e');
+        putchar(' ');
+        putchar('o');
+        putchar('f');
+        putchar(' ');
+        putchar('a');
+        putchar('r');
+        putchar('t');
+        putchar(' ');
+        putchar('i');
+        putchar('s');
+        putchar(' ');
+        putchar('u');
+        putchar('s');
+        putchar('e');
+        putchar('f');
+        putchar('u');
+        putchar('l');
+        putchar('"');
+
+	return (0);
+}
+
+/**
+ * printAuthor - to print the saying author
+ *
+ * Return: 0 void
+ */
+function printAuthor(void)
+{
+	putchar('D');
+        putchar('o');
+        putchar('r');
+        putchar('a');
+        putchar(' ');
+        putchar('K');
+        putchar('o');
+        putchar('r');
+        putchar('p');
+        putchar('a');
+        putchar('r');
+        putchar(',');
+        putchar(' ');
+        putchar('2');
+        putchar('0');
+        putchar('1');
+        putchar('5');
+        putchar('-');
+        putchar('1');
+        putchar('0');
+        putchar('-');
+        putchar('1');
+        putchar('9');
+
+	return (0);
 }
