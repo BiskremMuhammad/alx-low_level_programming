eef1bdb64f43caa804ae38347bc9fb83313949a4
Muhammad Omran <use.yurMind@gmail.com>
2023-06-20 08:52:05 -0700

    0x02-07 fn to get the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - fn to print the last digit of a number
+ * @n: the number
+ *
+ * Return: the last digit of the number
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
