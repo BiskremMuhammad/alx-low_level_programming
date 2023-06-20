6507eb5b3daa8a9daacbc5b5fdb3c5244426110d
Muhammad Omran <use.yurMind@gmail.com>
2023-06-20 05:21:28 -0700

    0x02-01 function to print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - to print alphabet to screen
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for(c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+}
+
+/**
+ * main - Entry Point
+ *
+ * Return: 0 void
+ */
+int main(void)
+{
+	print_alphabet();
+	putchar('\n');
+
+	return (0);
+}
