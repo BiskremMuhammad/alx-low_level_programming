e8da3aca802d2ec1450af28ae12daba3f8baed72
Muhammad Omran <use.yurMind@gmail.com>
2023-06-20 08:49:42 -0700

    0x02-06 fn to get the absolute value of a umber

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - fn to get the absolute value of a number
+ * @n: the number
+ *
+ * Return: the absolute value of the number
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
