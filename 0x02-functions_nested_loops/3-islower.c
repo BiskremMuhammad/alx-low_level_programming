0db6b1480bb687bc282f8e16e6281c750dd1a575
Muhammad Omran <use.yurMind@gmail.com>
2023-06-20 08:07:27 -0700

    0x02-03 check character is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - fn to check if character is lower case or not
+ * @c: the input character under check
+ *
+ * Return: 1 when character is lowe, 0 otherwise
+ */
+int _islower(char c)
+{
+	return islower(c) ? 1 : 0;
+}
