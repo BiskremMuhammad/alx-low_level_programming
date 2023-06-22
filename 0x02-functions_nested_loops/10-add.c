816572918a9a83bf4c5d44f9b3249f0addc030f6
Muhammad Omran <use.yurMind@gmail.com>
2023-06-22 05:10:01 -0700

    0x02-10 fn to add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - fn to add two numbers
+ * @a: first number
+ * @n: second number
+ *
+ * Return: {int} the result of adding the two numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
