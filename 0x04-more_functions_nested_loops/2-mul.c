87bf5d1b4e06cf81075099e4647522f015a526e4
Muhammad Omran <use.yurMind@gmail.com>
2023-06-23 04:54:46 -0700

    0x04-02 f to multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - fn to multiply two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: muliplication restuls
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
