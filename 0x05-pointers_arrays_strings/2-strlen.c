e14189445a4319036a273afbe1d0cc40b66bccde
Muhammad Omran <use.yurMind@gmail.com>
2023-06-26 15:05:18 -0700

    0x05-02 fn to get the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - fn to get length of a string
+ * @s: the string pointer
+ *
+ * Return: {int} the length of the string
+ */
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
