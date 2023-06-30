a66471ded834091b032acf3473a1163b6dce8af9
Muhammad Omran <use.yurMind@gmail.com>
2023-06-30 05:50:09 -0700

    0x06-03 fn to compare two string

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcmp - fn to compare two string
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Return: {int} the compareison result
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
