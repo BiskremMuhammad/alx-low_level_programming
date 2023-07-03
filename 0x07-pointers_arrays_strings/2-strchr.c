3006c2bd20fe774dd19c8406002f210c97003b8b
Muhammad Omran <use.yurMind@gmail.com>
2023-07-03 05:36:50 -0700

    0x07-02 fn to get character position on a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#inlcude "main.h"
+#include <string.h>
+
+/**
+ * _strchr - fn to return a character from a string on a position
+ * @s: the string
+ * @c: the character to get
+ *
+ * Return: pointer to the character's position
+ */
+char *_strchr(char *s, char c)
+{
+	char *p;
+
+	p = strchr(s, c);
+
+	return (p);
+}
