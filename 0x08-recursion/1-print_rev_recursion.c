e49923eaf2a1046d2b59ed0d5ece6213f5dd5e8e
Muhammad Omran <use.yurMind@gmail.com>
2023-07-05 06:19:23 -0700

    0x08-01 print a string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _print_rev_recursion - fn to print a string in reverse
+ * @s: the string
+ */
+void _print_rev_recursion(char *s)
+{
+	puts(strrev(s));
+}
