62aab0265f3e1dc003e92c8c9651df6ce4e67153
Muhammad Omran <use.yurMind@gmail.com>
2023-07-06 03:48:15 -0700

    0x08-04 fn to get the power y of a number

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <math.h>
 
 /**
  * _pow_recursion - fn to return result of x ^ y
@@ -10,5 +9,12 @@
  */
 int _pow_recursion(int x, int y)
 {
-	return (pow(x, y));
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else if (y == 1)
+		return (x);
+
+	return (x * y + _pow_recursion(x, y - 1));
 }
