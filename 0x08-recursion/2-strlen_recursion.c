1564dc46871d7ef1013989ece5be2350445d5af8
Muhammad Omran <use.yurMind@gmail.com>
2023-07-06 02:48:35 -0700

    0x08-02 fn to return the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen_recursion - fn to return string length
+ * @s: the string
+ *
+ * Return: the length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
