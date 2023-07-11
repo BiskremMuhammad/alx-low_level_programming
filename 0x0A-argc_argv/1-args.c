9bbbc7fd5f97d0de699afe28e82ab03cc5a3c24e
Muhammad Omran <use.yurMind@gmail.com>
2023-07-11 07:58:43 -0700

    0x0A-01 fn to print number of args passed to the program

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry Point
+ * @argc: script arguments count
+ * @argv: script passed arguments
+ *
+ * Return: {int}
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
