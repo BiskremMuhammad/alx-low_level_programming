9919604d1c71604faad81b945467311fdad0b005
Muhammad Omran <use.yurMind@gmail.com>
2023-07-11 15:53:27 -0700

    0x0B-00 fn to create dynamic size array filled with character c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * create_array - fn to create an array with dynamic size
+ * @size: size of the array
+ * @c: the character to fill the array with
+ *
+ * Return: {char} pointer to the allocated space
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *t;
+	int i;
+
+	if (size <= 0)
+		return (NULL);
+
+	t = malloc(sizeof(char) * size);
+	for (i = 0; i < size; i++)
+		t[i] = c;
+
+	return (t);
+}
