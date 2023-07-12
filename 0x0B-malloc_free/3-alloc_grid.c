d729da7208221381408d7cea94215f55eecb44c1
Muhammad Omran <use.yurMind@gmail.com>
2023-07-12 06:34:19 -0700

    0x0B-03 fn to allocate space for two dimentioanl array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * alloc_grid - fn to allocate space for two dimentional array
+ * @width: array width
+ * @height: array height
+ *
+ * Return: {int} pointer to the allocated space
+ */
+int **alloc_grid(int width, int height)
+{
+	int **t, i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	t = malloc(sizeof(int) * width * height);
+	if (t == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			t[i][j] = 0;
+	}
+
+	return (t);
+}
