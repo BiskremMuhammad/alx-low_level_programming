8b99a6c9366049c67222e7b7289daf6add479e04
Muhammad Omran <use.yurMind@gmail.com>
2023-07-14 04:22:13 -0700

    0x0C-00 fn to allocate space

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * malloc_checked - fn name
+ * @b: p1
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
