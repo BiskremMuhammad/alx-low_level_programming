d71fd93db785e3c7a107effaecec5418ceb3fae4
Muhammad Omran <use.yurMind@gmail.com>
2023-07-17 10:16:46 -0700

    0x0E-02 dog struct copy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,20 @@ struct dog
 	char *owner;
 };
 
+/**
+ * struct dog_t - dog copy
+ *
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+struct dog_t
+{
+	char *name;
+        float age;
+        char *owner;
+};
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
