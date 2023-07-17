0fc74961aaee567623035d3a53d2e1526b142cfd
Muhammad Omran <use.yurMind@gmail.com>
2023-07-17 10:24:34 -0700

    0x0E-04 fn to create a new dog object

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * new_dog - fn to create a new dog obj
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Return: {dog_t} pointer to the created dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *p;
+
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
+
+	return (p);
+}
