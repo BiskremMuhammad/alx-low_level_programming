1f2ed5ecb42a852cb8caef6ba418b831cdbbca95
Muhammad Omran <use.yurMind@gmail.com>
2023-07-17 06:08:59 -0700

    0x0E-01 fn to init a dog structure

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/**
+ * init_dog - fn to initialize a dog object
+ * @d: dog object pointer
+ * @name: name of the dog to change
+ * @age: the new age to change
+ * @owner: new dog owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
