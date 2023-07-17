5049d510a7aebe880f470f93eac09253e13ff90b
Muhammad Omran <use.yurMind@gmail.com>
2023-07-17 10:21:02 -0700

    0x0E-02 define type name for dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,19 +15,7 @@ struct dog
 	char *owner;
 };
 
-/**
- * struct dog_t - dog copy
- *
- * @name: dog name
- * @age: dog age
- * @owner: dog owner
- */
-struct dog_t
-{
-	char *name;
-        float age;
-        char *owner;
-};
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
