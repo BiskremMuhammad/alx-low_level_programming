2a3894e9eb10a6b069cf9ea723fc50de84a0b859
Muhammad Omran <use.yurMind@gmail.com>
2023-07-17 10:34:00 -0700

    0x0E-04 allocate space for dog obj

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,6 +14,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *p;
 
 	p = malloc(sizeof(dog_t));
+	if (p == NULL)
+		return (NULL);
+
 	p->name = name;
 	p->age = age;
 	p->owner = owner;
