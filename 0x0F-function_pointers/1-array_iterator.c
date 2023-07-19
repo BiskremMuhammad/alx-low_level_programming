1a5079eba869f31dea916331ce0ed5f75dcfa5c8
Muhammad Omran <use.yurMind@gmail.com>
2023-07-19 08:10:18 -0700

    0x0F-01 fn to iterate though an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - fn to iterate though array
+ * @array: the array
+ * @size: size of the array
+ * @action: an action fn pointer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+		(*action)(i)
+}
