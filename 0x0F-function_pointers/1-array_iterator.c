91d37f065c8b6b96fb07c3f1ca58c98f6239282e
Muhammad Omran <use.yurMind@gmail.com>
2023-07-19 08:16:53 -0700

    0x0F-01 check for NULLs

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,8 +8,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
+	if (array != NULL && action != NULL)
+	{
+		size_t i;
 
-	for (i = 0; i < size; i++)
-		(*action)(array[i]);
+		for (i = 0; i < size; i++)
+			(*action)(array[i]);
+	}
 }
