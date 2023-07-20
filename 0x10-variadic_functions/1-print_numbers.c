90c456b1d19a42286366a9ea5da6d7f8659a6c9b
Muhammad Omran <use.yurMind@gmail.com>
2023-07-20 06:34:31 -0700

    0x10-01 fn to print numbers joined by separator

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - variadic fn to print all passed numbers joined by separator
+ * @separator: separator between numbers
+ * @n: number of numbers passed as arguments
+ * @...: variable arguments
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list a;
+	unsigned int i;
+
+	if (n > 0)
+	{
+		va_start(a, n);
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(a, int));
+			if (separator != NULL && i < n - 1)
+				printf("%s", separator);
+		}
+		va_end(a);
+	}
+}
