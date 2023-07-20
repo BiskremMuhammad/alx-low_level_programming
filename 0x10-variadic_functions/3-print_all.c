e62782024040eb698dbbcf261a42180eed2e264b
Muhammad Omran <use.yurMind@gmail.com>
2023-07-20 08:40:10 -0700

    0x10-03 fix logic

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,6 @@
 #include "variadic_functions.h"
 #include <string.h>
+#include <stdbool.h>
 
 /**
  * print_all - variadic fn to print anything passed to it
@@ -10,35 +11,53 @@ void print_all(const char * const format, ...)
 {
 	va_list a;
 	size_t i;
+	int d;
+	char c;
+	float f;
+	bool v;
 
 	i = 0;
+	va_start(a, format);
 	while (i < strlen(format))
 	{
+		v = false;
 		switch (format[i])
 		{
 			case 'c':
-				printf("%c", va_arg(a, int));
+				c = (char)va_arg(a, int);
+				printf("%c", c);
+				v = true;
 				break;
 			case 'i':
-				printf("%d", va_arg(a, int));
+				d = va_arg(a, int);
+				printf("%d", d);
+				v = true;
 				break;
 			case 'f':
-				printf("%f", va_arg(a, double));
+				f = (float)va_arg(a, double);
+				printf("%f", f);
+				v = true;
 				break;
 			case 's':
 				{
 					char *str = va_arg(a, char *);
 
+					v = true;
 					if (str == NULL)
+					{
 						printf("(nil)");
-					if (str != NULL)
-						printf("%s", str);
+						break;
+					}
+					printf("%s", str);
 					break;
 				}
 			default:
 				break;
 		}
+
 		i++;
+		if (i < strlen(format) && v)
+			printf(", ");
 	}
 
 	printf("\n");
