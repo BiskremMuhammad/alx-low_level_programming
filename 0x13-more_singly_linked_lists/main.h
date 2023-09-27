34bcf66898266fbd32dd0480461d2167587ca881
Muhammad Omran <use.yurMind@gmail.com>
2023-09-27 15:53:36 -0700

    define print_listint fn

--- 0x13-more_singly_linked_lists/main.h
+++ 0x13-more_singly_linked_lists/main.h
@@ -1,6 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
 
 int _putchar(char c);
+size_t print_listint(const listint_t *h);
+
 #endif
