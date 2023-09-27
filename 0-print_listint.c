591fa2a3fdbf541baec6967a075c09d5c1dcc3ae
Muhammad Omran <use.yurMind@gmail.com>
2023-09-27 15:45:32 -0700

    0x13-00 print list

---
+++ 0-print_listint.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_listint - fn name
+ * @h: list header
+ *
+ * Return: {size_t} the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *cursor = h;
+	size_t c = 0;
+
+	while (cursor != NULL)
+	{
+		printf("%d\n", cursor->n);
+		c += 1;
+		cursor = cursor->next;
+	}
+	return (c);
+}
